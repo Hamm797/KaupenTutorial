815183cef22920fa361b6e88b5b7d464d48875fc
Hamm797 <jack.cannon@stu.boone.kyschools.us>
2025-06-22 15:45:56 -0400

    Initial commit

---
+++ src/main/java/com/mimic/kaupentutorial/KaupenTutorialClient.java
@@ -0,0 +1,8 @@
+package com.mimic.kaupentutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KaupenTutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
---
+++ src/main/java/com/mimic/kaupentutorial/KaupenTutorialDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mimic.kaupentutorial;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KaupenTutorialDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
